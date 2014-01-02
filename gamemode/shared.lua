ad6b95688448329d790ddcf8f86afefceed067b8
starman3 <jamesnshiffer@gmail.com>
2014-01-02 12:19:32 -0800

    Adding teams
    Trolls, Innocents, and Spectators.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -8,3 +8,8 @@ function GM:Initialize()
 	self.BaseClass.Initialize( self )
 	
 end
+
+//setting up teams
+team.SetUp(1, "The Trolls", Color(255,0,0))
+team.SetUp(2, "The Innocents", Color(0,0,255))
+team.SetUp(3, "Spectators", Color(192,192,192))

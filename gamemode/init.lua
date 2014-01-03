b1492fd2a0e7bee07e3a6c3073ec1f5a39ace834
starman3 <jamesnshiffer@gmail.com>
2014-01-02 19:04:23 -0800

    added rough team balancing
    I'm planning to have there be at least 3 players required to start a
    game, and 1/3 of the people will go to Innocents and the others in
    Trolls.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -12,30 +12,51 @@ include( 'shared.lua' )
    Desc: Give the player the default spawning weapons/ammo
 ---------------------------------------------------------*/
 function GM:PlayerInitialSpawn( ply )
-	ply:SetGravity(0.25)
-	ply:SetRunSpeed(750)
+	while table.Count(player.GetAll()) < 6 do
+		ply:SetTeam(3)
+	end
+	ply:SetTeam(3)
+	local rdmGen = math.random(1,2)
+		ply:SetTeam(rdmGen)
+	end
+
+	if trollPlayers > purePlayers then
+		ply:SetTeam(2)
+	elseif purePlayers > trollPlayers then
+		ply:SetTeam(1)
+	end
 end
 
 function GM:PlayerSpawn (ply)
-    ply:GiveAmmo(9999, "Pistol", false)
-    ply:GiveAmmo(9999, "357", false)
-    ply:GiveAmmo(9999, "SMG1", false)
-    ply:GiveAmmo(9999, "SMG1_Grenade", false)
-    ply:GiveAmmo(9999, "Buckshot", false)
-    ply:GiveAmmo(9999, "AR2", false)
-    ply:GiveAmmo(9999, "AR2AltFire", false)
-    ply:GiveAmmo(9999, "RPG_Round", false)
-    ply:GiveAmmo(9999, "XBowBolt", false)
-    ply:GiveAmmo(9999, "grenade", false)
-    
-    ply:Give("weapon_crowbar")
-    ply:Give("weapon_pistol")
-    ply:Give("weapon_357")
-    ply:Give("weapon_smg")
-    ply:Give("weapon_shotgun")
-    ply:Give("weapon_ar2")
-    ply:Give("weapon_rpg")
-    ply:Give("weapon_crossbow")
-    ply:Give("weapon_frag")
-    ply:Give("weapon_stunstick")
+	if ply:Team() == 1 then
+		ply:SetGravity(0.25)
+		ply:SetRunSpeed(750)
+		
+		ply:GiveAmmo(9999, "Pistol", false)
+		ply:GiveAmmo(9999, "357", false)
+		ply:GiveAmmo(9999, "SMG1", false)
+		ply:GiveAmmo(9999, "SMG1_Grenade", false)
+		ply:GiveAmmo(9999, "Buckshot", false)
+		ply:GiveAmmo(9999, "AR2", false)
+		ply:GiveAmmo(9999, "AR2AltFire", false)
+		ply:GiveAmmo(9999, "RPG_Round", false)
+		ply:GiveAmmo(9999, "XBowBolt", false)
+		ply:GiveAmmo(9999, "grenade", false)
+		
+		ply:Give("weapon_crowbar")
+		ply:Give("weapon_pistol")
+		ply:Give("weapon_357")
+		ply:Give("weapon_smg")
+		ply:Give("weapon_shotgun")
+		ply:Give("weapon_ar2")
+		ply:Give("weapon_rpg")
+		ply:Give("weapon_crossbow")
+		ply:Give("weapon_frag")
+		ply:Give("weapon_stunstick")
+	end
+	if ply:Team() == 2 then
+		ply:Give("weapon_357")
+		ply:Give("weapon_shotgun")
+	end
+	
 end
